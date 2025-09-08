972ae3ecb3ab0bfd12c0d92bd87baa77ee451a1b
danie1-parker <Daniel.Parker@dv.net>
2025-09-08 15:59:53 +0300

    dv-3311

--- apps/pay/src/views/payerForm/components/payerFormHeader/PayerFormHeader.vue
+++ apps/pay/src/views/payerForm/components/payerFormHeader/PayerFormHeader.vue
@@ -7,14 +7,16 @@
 
 <template>
 	<div class="timeline">
-		<div
-			v-for="(item, index) in timeline"
-			:key="item.id"
-			class="timeline__item"
-			:class="[{ 'opacity-item': !item.isActive }, { success: currentStep === 5 && timeline.length - 1 === index }]"
-		>
-			<span class="timeline__item-label">{{ item.id }}</span>
-			<span>{{ $t(item.label) }}</span>
+		<div class="timeline__inner">
+			<div
+				v-for="(item, index) in timeline"
+				:key="item.id"
+				class="timeline__item"
+				:class="[{ 'opacity-item': !item.isActive }, { success: currentStep === 5 && timeline.length - 1 === index }]"
+			>
+				<span class="timeline__item-label">{{ item.id }}</span>
+				<span class="timeline__item-text">{{ $t(item.label) }}</span>
+			</div>
 		</div>
 	</div>
 </template>
@@ -22,16 +24,44 @@
 <style scoped lang="scss">
 	.timeline {
 		display: flex;
-		align-items: center;
-		justify-content: space-between;
 		background-color: $form-background;
 		padding: 20px 24px;
 		border-radius: 16px;
 		width: 100%;
+		@include mediamax(1024) {
+			overflow-x: auto;
+			overflow-y: hidden;
+			width: calc(100vw - #{$padding-main * 2}px);
+			@supports (min-height: 100dvw) {
+				width: calc(100dvw - #{$padding-main * 2}px);
+			}
+		}
+		@include mediamax(768) {
+			padding: 16px 24px;
+		}
+		&__inner {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			width: 100%;
+			@include mediamax(1024) {
+				width: 1024px;
+				min-width: 1024px;
+			}
+			@include mediamax(768) {
+				display: flex;
+				align-items: center;
+				justify-content: unset;
+				gap: 40px;
+				width: 100%;
+				min-width: unset;
+			}
+		}
 		&__item {
 			display: flex;
 			align-items: center;
 			gap: 12px;
+			flex-shrink: 0;
 			&-label {
 				border-radius: 100%;
 				background-color: $label-header-form-background;
@@ -42,6 +72,17 @@
 				flex-shrink: 0;
 				font-size: 14px;
 				font-weight: 500;
+				@include mediamax(768) {
+					font-size: 12px;
+					width: 20px;
+					height: 20px;
+				}
+			}
+			&-text {
+				font-size: 16px;
+				@include mediamax(768) {
+					font-size: 14px;
+				}
 			}
 			&.opacity-item {
 				opacity: 0.2;
