dd40cbd42b265cd3f05f07df774d1665d4409801
danie1-parker <Daniel.Parker@dv.net>
2025-10-24 11:07:34 +0300

    fix-icon

--- src/components/icons/blockchains/IconWldEthereum.vue
+++ src/components/icons/blockchains/IconWldEthereum.vue
@@ -1,12 +1,26 @@
 <template>
 	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<mask id="mask0_7925_7508" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-			<path d="M0 0H24V24H0V0Z" fill="white"/>
+		<rect width="24" height="24" rx="12" fill="white"/>
+		<mask id="mask0_7925_7504" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="-4" y="-4" width="33" height="32">
+			<path d="M28.008 -3.99609H-3.98633V27.9982H28.008V-3.99609Z" fill="white"/>
 		</mask>
-		<g mask="url(#mask0_7925_7508)">
-			<path d="M21.2459 6.37866L11.8941 6.39344C8.79753 6.39344 6.28711 8.90386 6.28711 12.0004C6.28711 15.097 8.79753 17.6074 11.8941 17.6074H20.8997" stroke="black" stroke-width="2.24948" stroke-miterlimit="10" stroke-linecap="round"/>
-			<path d="M1.27539 12.0017H22.8775" stroke="black" stroke-width="2.24948" stroke-miterlimit="10" stroke-linecap="round"/>
-			<path d="M12.0022 1.12549C18.0084 1.12549 22.8775 5.99452 22.8775 12.0007C22.8775 18.007 18.0084 22.876 12.0022 22.876C5.99598 22.876 1.12695 18.007 1.12695 12.0007C1.12695 5.99452 5.99598 1.12549 12.0022 1.12549Z" stroke="black" stroke-width="2.24948" stroke-miterlimit="10" stroke-linecap="round"/>
+		<g mask="url(#mask0_7925_7504)">
+			<mask id="mask1_7925_7504" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+				<path d="M0 0H24V24H0V0Z" fill="white"/>
+			</mask>
+			<g mask="url(#mask1_7925_7504)">
+				<path d="M21.2459 6.37866L11.8941 6.39344C8.79753 6.39344 6.28711 8.90386 6.28711 12.0004C6.28711 15.097 8.79753 17.6074 11.8941 17.6074H20.8997" stroke="black" stroke-width="2.24948" stroke-miterlimit="10" stroke-linecap="round"/>
+				<path d="M1.27539 12.0017H22.8775" stroke="black" stroke-width="2.24948" stroke-miterlimit="10" stroke-linecap="round"/>
+				<path d="M12.0022 1.12549C18.0084 1.12549 22.8775 5.99452 22.8775 12.0007C22.8775 18.007 18.0084 22.876 12.0022 22.876C5.99598 22.876 1.12695 18.007 1.12695 12.0007C1.12695 5.99452 5.99598 1.12549 12.0022 1.12549Z" stroke="black" stroke-width="2.24948" stroke-miterlimit="10" stroke-linecap="round"/>
+			</g>
 		</g>
+		<rect x="14.5" y="14.5" width="9" height="9" rx="4.5" stroke="white"/>
+		<circle cx="19" cy="19" r="4" fill="#EDEFF1"/>
+		<path d="M17.334 19.0496L19.0002 20.0172V18.3056V16.3335L17.334 19.0496Z" fill="#898989"/>
+		<path d="M19 16.3335V18.3056V20.0172L20.6662 19.0496L19 16.3335Z" fill="#323232"/>
+		<path d="M19 18.3057L20.6657 19.0495L19 20.0168V18.3057Z" fill="#141414"/>
+		<path d="M19.0002 18.3057L19.0007 18.3058V20.0168L19.0002 20.0173L17.334 19.0497L19.0002 18.3057Z" fill="#373737"/>
+		<path d="M17.334 19.3601L19.0002 21.6669V20.3272L17.334 19.3601Z" fill="#898989"/>
+		<path d="M19 20.3272V21.6669L20.6672 19.3601L19 20.3272Z" fill="#323232"/>
 	</svg>
 </template>
