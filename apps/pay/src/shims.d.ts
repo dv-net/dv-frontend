c1b992fff6b888df2fe8673f75d8d054dc9df3e4
danie1-parker <Daniel.Parker@dv.net>
2025-10-28 17:31:58 +0300

    dv-3669

--- apps/pay/src/shims.d.ts
+++ apps/pay/src/shims.d.ts
@@ -2,17 +2,3 @@ declare module 'https://cdn.jsdelivr.net/npm/transliteration@2.3.5/dist/browser/
 	export function transliterate(text: string, options?: any): string;
 	export function slugify(text: string, options?: any): string;
 }
-
-declare global {
-	interface Window {
-		tronWeb: any;
-		tronLink: any;
-		ethereum: any;
-		okxwallet: {
-			tronLink?: {
-				request?: (params: any) => Promise<any>;
-				tronWeb?: any;
-			};
-		};
-	}
-}
