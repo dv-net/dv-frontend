71508fc4f3149c1268dad047207b71ad05805db9
Daniel Parker <daniel.parker@dv.net>
2026-02-02 12:22:33 +0300

    dv-4129 (#64)

--- apps/dv-admin/src/components/ui/tooltipHelper/TooltipHelper.vue
+++ apps/dv-admin/src/components/ui/tooltipHelper/TooltipHelper.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 	import { UiTooltip, UiIcon } from "@dv.net/ui-kit";
 	import type { IProps } from "./types";
+	import { useSlots } from "vue";
 
-	withDefaults(defineProps<IProps>(), {
-		position: "top-start",
-		iconSize: "md",
-		iconType: "filled"
-	});
+	const { iconSize = "md", iconType = "filled" } = defineProps<IProps>();
+
+	const slots = useSlots();
 </script>
 
 <template>
-	<ui-tooltip mode="dark" :title="title" :text="text" is-gold-title :popperClass="popperClass" :timeout="timeout">
-		<template #title><slot name="infoTitle" /></template>
-		<template #text><slot name="infoText" /></template>
+	<ui-tooltip :title="title" :text="text" :popperClass="popperClass" :timeout="timeout">
+		<template v-if="slots.infoTitle" #title><slot name="infoTitle" /></template>
+		<template v-if="slots.infoText" #text><slot name="infoText" /></template>
 		<ui-icon class="pointer" name="help" :type="iconType" :color="iconColor" :size="iconSize" />
 	</ui-tooltip>
 </template>
